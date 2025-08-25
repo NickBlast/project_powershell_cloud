911358926fc847dd681e9bdbd89c3ff877cce903
NickBlast <nlundquist@proton.me>
2025-08-25 17:59:40 -0500

    remove bloat files

--- .export_schema_test.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module "$PSScriptRoot/modules/export/Export.psm1"
-$s = Get-DatasetSchema -DatasetName 'entra_role_assignments'
-Write-Information "Schema version: $($s.version)"
-$sample = [pscustomobject]@{RoleId='r1';RoleDisplayName='Admin';MemberId='u1';MemberType='User';MemberDisplayName='User One';MemberPrincipalName='user@contoso.com'}
-$ok = Test-ObjectAgainstSchema -InputObject $sample -Schema $s
-Write-Information "Validation: $ok"
